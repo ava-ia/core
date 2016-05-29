34781545a9c235cd95bfa05bee91083439f15520
soyjavi <javi.jimenez.villar@gmail.com>
2016-05-29 20:56:35 +0700

    First version for get relations with NLP Compromise

--- adaptors/nlp/nlp.compromise.js
+++ adaptors/nlp/nlp.compromise.js
@@ -2,9 +2,24 @@
 'use strict';
 
 import Compromise from 'nlp_compromise';
+// -- Internal
 
-export default (request, ava) => {
+export default (phrase, ava) => {
+  const time = new Date();
+  const nlp = Compromise.text(phrase);
+  let relations = {};
+  nlp.sentences[0].terms.map((term) => {
+    let key = (term.pos.Verb ? 'verb' : term.tag).toLowerCase();
+    relations[key] = {
+      text: term.text,
+      info: term.pos,
+      tag: term.tag
+    }
+  });
   return new Promise((resolve, reject) => {
-    resolve(request);
+    resolve({
+      ms: (new Date() - time),
+      relations: relations
+    });
   });
 };
