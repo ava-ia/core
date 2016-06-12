624f431f1b35ba7a7ab546adfbc5a6a962fd0aa4
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-12 10:28:36 +0700

    Improve ProcessorRelations with JavaScript dates and tense of vebs.

--- ava/nlps/processors/processor.relations.js
+++ ava/nlps/processors/processor.relations.js
@@ -1,11 +1,10 @@
 'use strict';
 
 /* -- @TODO --------------------------------------------------------------------
-   - Don't use the same subject/object
-   - Use `adverbs` like `here` (for example for create where relation)
-   - Remove complement verbs or only get the first part of verb
-
-   - When use `it's` crash
+   - Don't use the same {subject}/{object}
+   - Use `noun` tag like `here` like a {location} relation
+   - Improve value: `3 buses` must be {value: 3} {object: bus}
+   - {when} must be correctly parse
 ----------------------------------------------------------------------------- */
 
 import Compromise from 'nlp_compromise';
@@ -13,6 +12,7 @@ import Chrono from 'chrono-node';
 // -- Internal
 const TERMS_RELATIONS = {
   person: 'subject',
+  adverb: 'adverb',
   verb: 'action',
   noun: 'object',
   date: 'when',
@@ -24,17 +24,17 @@ let lexicon = Compromise.lexicon()
 lexicon['ava'] = 'Person';
 
 export default (state) => {
-  const root_sentence = Compromise.text(state.sentence).root();
-  const terms = Compromise.text(root_sentence, {lexicon:lexicon}).sentences[0].terms;
+  const sentence = Compromise.text(state.sentence).normal();
+  const terms = Compromise.text(sentence, {lexicon}).sentences[0].terms;
   let relations = {};
 
   terms.map((term) => {
-    let key = (term.pos.Verb ? 'verb' : term.tag).toLowerCase();
+    let tag = (term.pos.Verb ? 'verb' : term.tag).toLowerCase();
     let relation;
 
-    if (relation = TERMS_RELATIONS[key]) {
+    if (relation = TERMS_RELATIONS[tag]) {
       if (relation === TERMS_RELATIONS.person && relations[relation]) relation = TERMS_RELATIONS.noun;
-      relations[relation] = extractRelation(key, term);
+      relations[relation] = extractRelation(tag, term, relations[relation]);
     }
   });
   state.nlp.relations = relations;
@@ -43,16 +43,35 @@ export default (state) => {
 };
 
 // -- Private methods
-const extractRelation = (key, term) => {
-  let tag;
-  let verb;
-  let value = term.normal && term.text;
-  if (key === 'date') value = (Chrono.parseDate(term.text) || '').toString();
-  if (key === 'verb') {
-    value = Compromise.verb(term.text).conjugate().infinitive;
-    verb = {text: term.text, tense: term.tag.split('Tense')[0].toLowerCase()};
-  } else {
-    tag = term.tag;
+const extractRelation = (tag, term, previous) => {
+  let relation = {};
+  let text = term.normal || term.text;
+
+  switch(tag) {
+    case 'date':
+      text = Chrono.parseDate(text)
+      break;
+
+    case 'verb':
+      const compromiseVerb = Compromise.verb(term.expansion || term.text);
+      if (!previous) {
+        relation.verb = {
+          tense: compromiseVerb.conjugation().toLowerCase().split('tense')[0],
+          negative: compromiseVerb.isNegative()
+        };
+      } else {
+        relation.verb = previous.verb;
+      }
+      text = compromiseVerb.conjugate().infinitive;
+      break;
+
+    case 'person':
+      tag = term.pos.Pronoun ? 'pronoun' : tag;
+      break;
   }
-  return { text: value, verb: verb, tag: tag };
+
+  relation.tag = tag;
+  relation.text = text;
+
+  return relation;
 };
---
+++ test/processors/processor.relations.spec.js
@@ -0,0 +1,83 @@
+import { assert, expect, should } from 'chai';
+import { Relations } from 'ava/nlps/processors';
+import colors from 'colors';
+
+// -- Mock
+
+describe('ProcessorRelations', () => {
+
+  let state;
+
+  beforeEach( () => state = {sentence: '', nlp: {}})
+
+  it('Up & Running', () => {
+    state.sentence = 'hello world';
+    const relations = Relations(state).nlp.relations;
+
+    expect(Object.keys(relations).length).equal(1);
+    expect(relations).to.have.all.keys('object');
+
+    expect(relations.object).to.have.all.keys('text', 'tag');
+    expect(relations.object.text).equal('world');
+    expect(relations.object.tag).equal('noun');
+  });
+
+  it('Analize complex sentences', () => {
+    state.sentence = "Ava, Do you know if tomorrow will rain in Bangkok?"
+    const relations = Relations(state).nlp.relations;
+
+    expect(relations).to.have.all.keys('subject', 'action', 'when', 'object', 'location');
+
+    expect(relations.subject).to.have.all.keys('text', 'tag');
+    expect(relations.subject.text).equal('ava');
+    expect(relations.subject.tag).equal('person');
+
+    expect(relations.action).to.have.all.keys('text', 'tag', 'verb');
+    expect(relations.action.text).equal('will');
+    expect(relations.action.tag).equal('verb');
+
+    expect(relations.when).to.have.all.keys('text', 'tag');
+    // expect(relations.when.text).equal('Sun Jun 12 2016 12:00:00 GMT+0700 (ICT)');
+    expect(relations.when.tag).equal('date');
+
+    expect(relations.object).to.have.all.keys('text', 'tag');
+    expect(relations.object.text).equal('rain');
+    expect(relations.object.tag).equal('noun');
+
+    expect(relations.location).to.have.all.keys('text', 'tag');
+    expect(relations.location.text).equal('bangkok');
+    expect(relations.location.tag).equal('place');
+  });
+
+  it('Can detect if action is a past tense', () => {
+    state.sentence = "I was there"
+    const relations = Relations(state).nlp.relations;
+
+    expect(relations.action).to.have.all.keys('text', 'tag', 'verb');
+    expect(relations.action.verb.tense).equal('past');
+  });
+
+  it('Can detect if action is a future tense', () => {
+    state.sentence = "I will be there"
+    const relations = Relations(state).nlp.relations;
+
+    expect(relations.action).to.have.all.keys('text', 'tag', 'verb');
+    expect(relations.action.verb.tense).equal('future');
+  });
+
+  it('Can detect if action is negative', () => {
+    state.sentence = "I won't be there"
+    const relations = Relations(state).nlp.relations;
+
+    expect(relations.action).to.have.all.keys('text', 'tag', 'verb');
+    expect(relations.action.verb.negative).equal(true);
+  });
+
+  it('Can detect dates and parse to JavaScript type', () => {
+    state.sentence = "I will be there tomorrow at 2pm"
+    const relations = Relations(state).nlp.relations;
+
+    expect(relations.when.text instanceof Date).equal(true)
+  });
+
+});
