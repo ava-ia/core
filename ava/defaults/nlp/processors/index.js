be528224ef382f1388150be9e8e787c17ce1b708
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-12 20:02:05 +0700

    Fix how we export processors

--- ava/defaults/nlp/processors/index.js
+++ ava/defaults/nlp/processors/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-export Taxonomy from './processor.taxonomy';
-export Compromise from './processor.compromise';
-export Relations from './processor.relations';
-export Sentiment from './processor.sentiment';
+import Alchemy from './processor.alchemy';
+import Compromise from './processor.compromise';
+import Relations from './processor.relations';
+import Salient from './processor.salient';
+
+export { Alchemy, Compromise, Relations, Salient };
