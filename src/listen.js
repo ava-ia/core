563221f066a624673c78363fe94dda0437df8873
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-17 21:13:52 +0700

    Use a default timeout (using config.json)

--- src/listen.js
+++ src/listen.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import { composeAsync, factoryComposers, factoryIntents } from './helpers'
+import { composeAsync, factoryComposers, factoryIntents, timeout } from './helpers'
 
 export default (state) => ({
   listen: (sentence) => {
     return new Promise( (resolve, reject) => {
       state.rawSentence = sentence;
 
+      timeout(reject);
       const factory = composeAsync(factoryComposers, factoryIntents);
 
       factory(state)
@@ -14,7 +15,7 @@ export default (state) => ({
           state.action ? resolve(state) : reject(new Error('No action'))
         })
         .catch ( error => {
-          if (!error) error = {code: 0, message: "Sorry, I haven't understood you"};
+          if (!error) error = { code: 0, message: "Sorry, I haven't understood you" };
           reject(error);
         })
     });
