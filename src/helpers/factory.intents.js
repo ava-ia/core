b296a3bf3c1a94873dd4d45acf0b3a570ebc7a87
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-17 20:12:37 +0700

    Don't need create a new promise, 'cause is better use async/await

--- src/helpers/factory.intents.js
+++ src/helpers/factory.intents.js
@@ -1,12 +1,7 @@
 'use strict';
 
-export default (state) => {
-  return new Promise( (resolve, reject) => {
-    state.action = undefined;
-    // -- @TODO: This execute all intents, how can we wait for the first successful one?
-    const intents = state.intents.map( (intent) => intent.script(state, intent) );
-    Promise.all(intents)
-      .then( state => resolve(state) )
-      .catch( error => reject(error) )
-  });
+export default async (state) => {
+  const intents = state.intents.map( intent => intent.script(state, intent) );
+
+  return await Promise.race(intents);
 }
