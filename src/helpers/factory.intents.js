f0226e33eb1e130343f79859c87d4e697042048f
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-18 16:02:43 +0700

    Fixed typo

--- src/helpers/factory.intents.js
+++ src/helpers/factory.intents.js
@@ -2,6 +2,7 @@
 
 export default async (state) => {
   const intents = state.intents.map( intent => intent.script(state, intent) );
+  state.action = undefined;
 
   return await Promise.race(intents);
 }
--- src/intents/intent.weather.js
+++ src/intents/intent.weather.js
@@ -5,7 +5,6 @@ import { factoryActions, intersect } from '../helpers'
 const TERMS = ['weather', 'umbrella', 'rain', 'forecast', 'snow', 'fog', 'sun', 'cloud', 'meteo'];
 
 export default (state, intent) => {
-
   const tokens = intersect(TERMS, state.nlp.tokens);
   const classifiers = intersect(TERMS, state.classifier.categories);
   console.log('IntentWeather'.bold.green, `tokens: ${tokens.toString().green}, classifiers: ${classifiers.toString().green}`);
