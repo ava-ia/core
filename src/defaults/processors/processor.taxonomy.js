7b5a646020eee39a7bee85a12518c739cb785241
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-18 08:40:35 +0700

    Bug fixed: Control if processor retrieves an array of taxonomies.

--- src/defaults/processors/processor.taxonomy.js
+++ src/defaults/processors/processor.taxonomy.js
@@ -13,6 +13,8 @@ export default (state) => {
 
   return new Promise((resolve, reject) => {
     processor.taxonomies(state.sentence, {}, (error, response) => {
+      if (error || !response.taxonomy) return resolve(state);
+
       const taxonomy = response.taxonomy[0];
       if (taxonomy) {
         state.nlp.taxonomy = (taxonomy.label.charAt(0) === '/') ? taxonomy.label.slice( 1 ) : taxonomy.label;
