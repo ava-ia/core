60427c259e590bd4300d6356f7ae7c5494113caa
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-27 18:24:58 +0700

    Improved syntax method

--- src/helpers/syntax.js
+++ src/helpers/syntax.js
@@ -14,23 +14,26 @@ export default (sentence, rules) => {
     if (matches.length > 0 && matches[0] !== null) {
       const terms = matches[0].terms;
       let values = {};
-      let keys = rule.match(regex).map( key => {
-        key = key.slice(1, -1).toLowerCase();
-        for (const term of terms) {
-          if ( term.tag.toLowerCase() === key) {
-            if (!values[key]) {
-              values[key] = term.text;
-              break;
-            } else {
-              if (!Array.isArray(values[key])) values[key] = [values[key]]
-              if (values[key].indexOf(term.text) === -1) {
-                values[key].push(term.text);
+      let keys = rule.match(regex);
+      if (keys) {
+        keys.map( key => {
+          key = key.slice(1, -1).toLowerCase();
+          for (const term of terms) {
+            if ( term.tag.toLowerCase() === key) {
+              if (!values[key]) {
+                values[key] = term.text;
                 break;
+              } else {
+                if (!Array.isArray(values[key])) values[key] = [values[key]]
+                if (values[key].indexOf(term.text) === -1) {
+                  values[key].push(term.text);
+                  break;
+                }
               }
             }
           }
-        }
-      });
+        });
+      }
 
       match = Object.keys(values).length > 0 ? values : matches[0].text();
       break;
