3cf94aac823df283a4f56b7d49af7914558bfaee
@soyjavi <javi.jimenez.villar@gmail.com>
2016-06-27 09:14:54 +0700

    Improve helper `syntax` for get values

--- src/helpers/syntax.js
+++ src/helpers/syntax.js
@@ -1,16 +1,33 @@
 'use strict';
 
 import Compromise from 'nlp_compromise';
+const regex = /\[(.*?)\]/g;
 
 export default (sentence, rules) => {
   if (!Array.isArray(rules)) rules = [rules];
   let match;
   const rootSentence = Compromise.text(sentence).root();
 
-  rules.map( rule => {
+  for (const rule of rules) {
     const matches = Compromise.text(rootSentence).match(rule);
-    if (matches.length > 0 && matches[0] !== null) match = matches[0].text()
-  });
+
+    if (matches.length > 0 && matches[0] !== null) {
+      const terms = matches[0].terms;
+      let values = [];
+
+      let keys = rule.match(regex).map( key => {
+        key = key.slice(1, -1).toLowerCase();
+        for (const term of terms) {
+          if ( term.tag.toLowerCase() === key && values.indexOf(term.text) === -1) {
+            values.push(term.text);
+            break;
+          }
+        }
+      });
+      match = values.length > 0 ? values : matches[0].text();
+      break;
+    }
+  }
 
   return (match);
 };
