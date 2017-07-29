f0d07685c3a70ccd88a76ab30e7021bad81398b5
@soyjavi <javi.jimenez.villar@gmail.com>
2017-07-29 10:34:15 +0700

    #improvement sentence to translate

--- src/actions/action.translator.js
+++ src/actions/action.translator.js
@@ -1,34 +1,39 @@
-import countries from 'world-countries'
-import Compromise from 'nlp_compromise'
-import googleTranslate from 'google-translate-api'
-import { entities } from '../helpers'
+import countries from 'world-countries';
+import Compromise from 'nlp_compromise';
+import googleTranslate from 'google-translate-api';
+import { entities } from '../helpers';
 // -- Internal
-const DEMONYM = 'Demonym'
-const PREPOSITION = 'Preposition'
+const DEMONYM = 'Demonym';
+const PREPOSITION = 'Preposition';
 
 export default (state) => {
 
   return new Promise((resolve, reject) => {
-    const actionIndex = state.tokens.indexOf('translate')
-    const terms = Compromise.text(state.sentence).sentences[0].terms
-    const ms = new Date()
-    let to
-    let sentence = ''
+    const actionIndex = state.tokens.indexOf('translate');
+    const terms = Compromise.text(state.sentence).sentences[0].terms;
+    const ms = new Date();
+    let to;
+    let demonymIndex;
+    let sentence = '';
 
     terms.map((term, index) => {
       if (index > actionIndex) {
-        if (term.tag === DEMONYM) {
-          const demonym = term.text.toLowerCase()
-          const country = countries.find(item => item.demonym.toLowerCase() === demonym)
-          if (country) to = country.cca2
-        } else if (!(term.tag === PREPOSITION && terms[index + 1].tag === DEMONYM)) {
-          sentence += `${term.text} `
+        if (term.tag === DEMONYM && demonymIndex === undefined) {
+          const demonym = term.text.toLowerCase();
+          const country = countries.find(item => item.demonym.toLowerCase() === demonym);
+
+          if (country) {
+            demonymIndex = index;
+            to = country.cca2;
+          }
+        } else if (index > demonymIndex) {
+          sentence += `${term.text} `;
         }
       }
-    })
+    });
 
     if (state.debug) {
-      console.log('ActionTranslator'.bold.yellow, 'sentence:'.bold, sentence, 'to:'.bold,  to)
+      console.log('ActionTranslator'.bold.yellow, 'sentence:'.bold, sentence, 'to:'.bold, to);
     }
 
     if (sentence && to) {
@@ -38,13 +43,13 @@ export default (state) => {
             engine: 'google',
             ms: (new Date() - ms),
             entity: entities.knowledge,
-            value: response.text
-          }
-          resolve(state)
+            value: response.text,
+          };
+          resolve(state);
         })
-        .catch(reject)
+        .catch(reject);
     } else {
-      resolve(state)
+      resolve(state);
     }
-  })
-}
+  });
+};
